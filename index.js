3fcbad82e0ba78acd28355b30ca4f963cc734595
Kamau-sam <skamau.6411@gmail.com>
2024-03-27 07:05:16 +0300

    All tests Passed

--- index.js
+++ index.js
@@ -1,1 +1,37 @@
-// Write your solution here!
+const cats = ["Milo", "Otis", "Garfield"];
+function destructivelyAppendCat(Ralph) {
+  cats.push("Ralph");
+}
+function destructivelyPrependCat(Mary) {
+  cats.unshift("Bob");
+}
+function destructivelyRemoveLastCat(Garfield) {
+  cats.pop("Garfield");
+}
+function destructivelyRemoveFirstCat() {
+  cats.shift("Milo");
+}
+function appendCat(Broom) {
+  const cats = ["Milo", "Otis", "Garfield"];
+  const copyCats = cats.slice();
+  copyCats.push("Broom");
+  return copyCats;
+}
+function prependCat(Arnold) {
+  const cats = ["Milo", "Otis", "Garfield"];
+  const myCats = cats.slice();
+  myCats.unshift("Arnold");
+  return myCats;
+}
+function removeLastCat() {
+  const cats = ["Milo", "Otis", "Garfield"];
+  const lessCats = cats.slice();
+  lessCats.pop();
+  return lessCats;
+}
+function removeFirstCat() {
+  const cats = ["Milo", "Otis", "Garfield"];
+  const lessLastCat = cats.slice();
+  lessLastCat.shift();
+  return lessLastCat;
+}
